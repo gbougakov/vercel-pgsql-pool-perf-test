02f8988331fa5508c3d595869d5f1c5416d895ff
George Bougakov <g@bygeorgenet.me>
2021-07-02 20:03:19 +0300

    Redeploy

--- api/data.js
+++ api/data.js
@@ -4,7 +4,7 @@ const { sql } = require('slonik')
 module.exports = async (req, res) => {
   const data = await pool.one(sql`select * from test_data where id = ${req.query.id};`)
   res.json({
-    disclaimer: 'Warning! This data is absolutely fake and does not belong to any person. All coincidences with any person, living or dead, are incidental.',
+    disclaimer: 'Warning! This data is absolutely fake and does not belong to any person. All coincidences with any person, living or dead, are incidental',
     data
   })
 }
